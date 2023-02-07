d9bf9d9367350faba13612f0fe7134120330694d
GlassAlo <maxence.labourel@epitech.eu>
2023-02-07 17:31:37 +0100

    [Mod] Rename Graph -> Circuit Updated the system to compute the values Added a level of abstraction for Input and Clock component

--- src/components/Clock.cpp
+++ src/components/Clock.cpp
@@ -7,22 +7,13 @@
 
 #include "Clock.hpp"
 
-nts::component::Clock::Clock(const std::string &name, std::vector<std::pair<std::vector<std::size_t>, std::vector<std::size_t>>> pins) : nts::AComponent(name, pins)
+nts::component::Clock::Clock(const std::string &name, std::vector<std::pair<std::vector<std::size_t>, std::vector<std::size_t>>> pins) : nts::AShell(name, pins)
 {
     _type = nts::CompType::CLOCK;
-    for (auto &pin : pins)
-        for (auto &output : pin.second)
-            _outputs[output] = nts::UNDEFINED;
-}
-
-nts::component::Clock::~Clock()
-{
 }
 
 nts::Tristate nts::component::Clock::compute(std::size_t pin)
 {
-    if (pin > _outputs.size())
-        return nts::UNDEFINED;
     return _outputs[pin];
 }
 
