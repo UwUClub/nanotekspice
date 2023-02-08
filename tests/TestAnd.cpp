dddd1f4005e14cbc3b507b47fdc28ce366db2d4e
Matheo <matheo.martin@epitech.eu>
2023-02-08 09:04:37 +0100

    [Upload] add everything to create test

---
+++ tests/TestAnd.cpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2023
+** nanotekspice
+** File description:
+** TestAnd.cpp
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "And.hpp"
+#include "Parser.hpp"
+#include "Error.hpp"
+
+Test(And, ErrorHandling, .init = cr_redirect_stdout)
+{
+    int ac = 2;
+    char *av[2] = {(char *)"./nanotekspice", (char *)"tests/Test.circ"};
+    try {
+        nts::Parser p(ac, av);
+        std::cout << "Test passed" << std::endl;
+    }
+    catch (nts::Error &e) {
+        std::cout << e.what() << std::endl;
+        cr_assert_eq(e.what(), "Invalid file extension (Parser.cpp line 24)");
+    }
+}
