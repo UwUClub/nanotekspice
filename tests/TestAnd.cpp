2dbf96f9bb3149fb0c21859f5b84e29e67c7fe00
Matheo <matheo.martin@epitech.eu>
2023-02-09 16:03:40 +0100

    [Upload] add all tests for And

--- tests/TestAnd.cpp
+++ tests/TestAnd.cpp
@@ -9,56 +9,164 @@
 #include <criterion/redirect.h>
 #include "Parser.hpp"
 #include "Error.hpp"
+#include "Handler.hpp"
 
-Test(And, ErrorHandling, .init = cr_redirect_stdout)
+Test(And, And, .init = cr_redirect_stdout)
 {
     int ac = 2;
-    char *av[2] = {(char *)"./nanotekspice", (char *)"tests/Test.circ"};
-    try {
-        nts::Parser p(ac, av);
-    }
-    catch (nts::Error &e) {
-        cr_assert_str_eq(e.what(), "Invalid file extension (Parser.cpp line 24)");
-    }
+    char *av[2] = {(char *)"./nanotekspice", (char *)"tests/nts_single/and.nts"};
+    nts::Parser parser(ac, av);
+    auto &f_cin = criterion::get_redirected_cin();
+    f_cin << "display" << std::endl;
+    f_cin << "in_1=1" << std::endl;
+    f_cin << "in_2=1" << std::endl;
+    f_cin << "display" << std::endl;
+    f_cin << "simulate" << std::endl;
+    f_cin << "display" << std::endl;
+    f_cin << "exit" << std::endl;
+    f_cin.close();
+
+    parser.getComponents();
+    parser.createChipsets();
+    parser.createLinks();
+    nts::Handler::runLoop();
+    cr_assert_stdout_eq_str("> Tick: 0\n"
+                            "Inputs:\n"
+                            "\tin_1: -1\n"
+                            "\tin_2: -1\n"
+                            "Outputs:\n"
+                            "\tout: -1\n"
+                            "> > > Tick: 0\n"
+                            "Inputs:\n"
+                            "\tin_1: -1\n"
+                            "\tin_2: -1\n"
+                            "Outputs:\n"
+                            "\tout: -1\n"
+                            "> > Tick: 1\n"
+                            "Inputs:\n"
+                            "\tin_1: 1\n"
+                            "\tin_2: 1\n"
+                            "Outputs:\n"
+                            "\tout: 1\n"
+                            "> ");
 }
 
-Test(And, ErrorHandling2)
+Test(And, And2, .init = cr_redirect_stdout)
 {
     int ac = 2;
-    char *av[2] = {(char *)"./nanotekspice", (char *)"tests/test.nts"};
-    try {
-        nts::Parser p(ac, av);
-    }
-    catch (nts::Error &e) {
-        cr_assert_str_eq(e.what(), "Invalid file (Parser.cpp line 26)");
-    }
+    char *av[2] = {(char *)"./nanotekspice", (char *)"tests/nts_single/and.nts"};
+    nts::Parser parser(ac, av);
+    auto &f_cin = criterion::get_redirected_cin();
+    f_cin << "display" << std::endl;
+    f_cin << "in_1=1" << std::endl;
+    f_cin << "in_2=0" << std::endl;
+    f_cin << "display" << std::endl;
+    f_cin << "simulate" << std::endl;
+    f_cin << "display" << std::endl;
+    f_cin << "exit" << std::endl;
+    f_cin.close();
+
+    parser.getComponents();
+    parser.createChipsets();
+    parser.createLinks();
+    nts::Handler::runLoop();
+    cr_assert_stdout_eq_str("> Tick: 0\n"
+                            "Inputs:\n"
+                            "\tin_1: -1\n"
+                            "\tin_2: -1\n"
+                            "Outputs:\n"
+                            "\tout: -1\n"
+                            "> > > Tick: 0\n"
+                            "Inputs:\n"
+                            "\tin_1: -1\n"
+                            "\tin_2: -1\n"
+                            "Outputs:\n"
+                            "\tout: -1\n"
+                            "> > Tick: 1\n"
+                            "Inputs:\n"
+                            "\tin_1: 1\n"
+                            "\tin_2: 0\n"
+                            "Outputs:\n"
+                            "\tout: 0\n"
+                            "> ");
 }
 
-Test(And, ErrorHandling3)
+Test(And, And3, .init = cr_redirect_stdout)
 {
     int ac = 2;
-    char *av[2] = {(char *)"./nanotekspice", (char *)"tests/test_no_chipsets.nts"};
-    try {
-        nts::Parser p(ac, av);
-    }
-    catch (nts::Error &e) {
-        cr_assert_str_eq(e.what(), "No chipsets in file (Parser.cpp line 42)");
-    }
+    char *av[2] = {(char *)"./nanotekspice", (char *)"tests/nts_single/and.nts"};
+    nts::Parser parser(ac, av);
+    auto &f_cin = criterion::get_redirected_cin();
+    f_cin << "display" << std::endl;
+    f_cin << "in_1=0" << std::endl;
+    f_cin << "in_2=1" << std::endl;
+    f_cin << "display" << std::endl;
+    f_cin << "simulate" << std::endl;
+    f_cin << "display" << std::endl;
+    f_cin << "exit" << std::endl;
+    f_cin.close();
+
+    parser.getComponents();
+    parser.createChipsets();
+    parser.createLinks();
+    nts::Handler::runLoop();
+    cr_assert_stdout_eq_str("> Tick: 0\n"
+                            "Inputs:\n"
+                            "\tin_1: -1\n"
+                            "\tin_2: -1\n"
+                            "Outputs:\n"
+                            "\tout: -1\n"
+                            "> > > Tick: 0\n"
+                            "Inputs:\n"
+                            "\tin_1: -1\n"
+                            "\tin_2: -1\n"
+                            "Outputs:\n"
+                            "\tout: -1\n"
+                            "> > Tick: 1\n"
+                            "Inputs:\n"
+                            "\tin_1: 0\n"
+                            "\tin_2: 1\n"
+                            "Outputs:\n"
+                            "\tout: 0\n"
+                            "> ");
 }
 
-Test(And, ErrorHandling4)
+Test(And, And4, .init = cr_redirect_stdout)
 {
     int ac = 2;
-    char *av[2] = {(char *)"./nanotekspice", (char *)"tests/test_no_links.nts"};
-    try {
-        nts::Parser p(ac, av);
-    }
-    catch (nts::Error &e) {
-        cr_assert_str_eq(e.what(), "No links in file (Parser.cpp line 65)");
-        std::cout << "Test passed" << std::endl;
-    }
-    catch (nts::Error &e) {
-        std::cout << e.what() << std::endl;
-        cr_assert_str_eq(e.what(), "Invalid file extension (Parser.cpp line 24)");
-    }
+    char *av[2] = {(char *)"./nanotekspice", (char *)"tests/nts_single/and.nts"};
+    nts::Parser parser(ac, av);
+    auto &f_cin = criterion::get_redirected_cin();
+    f_cin << "display" << std::endl;
+    f_cin << "in_1=0" << std::endl;
+    f_cin << "in_2=0" << std::endl;
+    f_cin << "display" << std::endl;
+    f_cin << "simulate" << std::endl;
+    f_cin << "display" << std::endl;
+    f_cin << "exit" << std::endl;
+    f_cin.close();
+
+    parser.getComponents();
+    parser.createChipsets();
+    parser.createLinks();
+    nts::Handler::runLoop();
+    cr_assert_stdout_eq_str("> Tick: 0\n"
+                            "Inputs:\n"
+                            "\tin_1: -1\n"
+                            "\tin_2: -1\n"
+                            "Outputs:\n"
+                            "\tout: -1\n"
+                            "> > > Tick: 0\n"
+                            "Inputs:\n"
+                            "\tin_1: -1\n"
+                            "\tin_2: -1\n"
+                            "Outputs:\n"
+                            "\tout: -1\n"
+                            "> > Tick: 1\n"
+                            "Inputs:\n"
+                            "\tin_1: 0\n"
+                            "\tin_2: 0\n"
+                            "Outputs:\n"
+                            "\tout: 0\n"
+                            "> ");
 }
